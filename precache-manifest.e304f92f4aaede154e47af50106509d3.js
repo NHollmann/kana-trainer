d04ba268b3fa017785dbc5d7993e306e9abbd7ad
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2019-01-09 22:52:46 +0100

    Updates

---
+++ precache-manifest.e304f92f4aaede154e47af50106509d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kana-trainer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d3ea86a741fa89ac9e6b",
+    "url": "/kana-trainer/static/js/runtime~main.d3ea86a7.js"
+  },
+  {
+    "revision": "a54ef64cd44d6f056e13",
+    "url": "/kana-trainer/static/js/main.a54ef64c.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/kana-trainer/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a54ef64cd44d6f056e13",
+    "url": "/kana-trainer/static/css/main.3cfb1517.chunk.css"
+  },
+  {
+    "revision": "9fde60a3c027ffacfccdb53010317cdf",
+    "url": "/kana-trainer/index.html"
+  }
+];
