47701ea2c437420fba9e48f17d7f2502da82ea68
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2019-01-12 00:47:43 +0100

    Added space key as enter alias

--- src/components/Typer.js
+++ src/components/Typer.js
@@ -15,7 +15,7 @@ class Typer extends Component {
         let newText = this.state.text;
         if (keyName === 'backspace') {
             newText = newText.slice(0, -1);
-        } else if (keyName === 'enter') {
+        } else if (keyName === 'enter' || keyName === 'space') {
             if (typeof this.props.onEnter === "function") {
                 this.props.onEnter(newText);
             }
@@ -32,7 +32,7 @@ class Typer extends Component {
     render() {
         return (
             <Hotkeys
-                keyName="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,backspace,enter"
+                keyName="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,backspace,enter,space"
                 onKeyDown={this.onKeyDown.bind(this)}
             >
                 <p className="output">{this.state.text}<span className="cursor">|</span></p>
