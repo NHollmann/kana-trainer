4b83de8015057c20b4a77e80b5ba7984b0c6c051
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2019-03-02 18:03:24 +0100

    Removed unused packages

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AppLayout from '../common/AppLayout';
+import InfoCard from '../common/InfoCard';
 
 function Home() {
     return (
         <AppLayout title="Home">
-
+            <InfoCard title="Kana Trainer Home">
+                On this page you later find quick menus and some usage statistics.
+            </InfoCard>
         </AppLayout>
     );
 }
